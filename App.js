0ce09541ebb56b7136e21903568b3f1433758057
AbhiAtiUttam <Abhi@glocalize.net>
2024-09-08 17:56:57 +0530

    finalized app

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { Provider } from 'react-redux';
+import store from './store';
+import StackNavigator from './StackNavigator';
+
+export default function App() {
+
+  return (
+    <Provider store={store}>
+      <StackNavigator />
+      <StatusBar style="auto" />
+    </Provider>
+
+
+  );
+}
+
